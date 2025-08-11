f9834fd8b1af148d2cbad6506eef8d807b02a778
windlite <128964303+windlite3@users.noreply.github.com>
2025-08-12 00:52:12 +0800

    Update evolution_lavender.mk

--- evolution_lavender.mk
+++ evolution_lavender.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common EvolutionX stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
